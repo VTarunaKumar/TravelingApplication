311d23559ac484507f0c4cbcfcdb3a82c21eb989
VTarunaKumar <vtarunakumarrao@gmail.com>
2022-10-12 18:42:49 +0530

    Completed

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit_states.dart';
 import 'package:travel_app/cubit/app_cubits.dart';
 import 'package:travel_app/misc/colors.dart';
 import 'package:travel_app/widgets/appButtons.dart';
@@ -18,198 +19,202 @@ class _DetailPageState extends State<DetailPage> {
   int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.maxFinite,
-                height: 300,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("img/mountain.jpeg"),
-                        fit: BoxFit.cover)),
+    return BlocBuilder<AppCubit, CubitStates>(builder: (context, state) {
+      DetailState detail = state as DetailState;
+      return Scaffold(
+        body: Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "http://mark.bslmeiyu.com/uploads/" +
+                                  detail.places.img),
+                          fit: BoxFit.cover)),
+                ),
               ),
-            ),
-            Positioned(
+              Positioned(
+                  left: 20,
+                  top: 60,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          BlocProvider.of<AppCubit>(context).goHome();
+                        },
+                        icon: Icon(Icons.menu),
+                        color: Colors.white,
+                      )
+                    ],
+                  )),
+              Positioned(
+                  top: 260,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                    width: MediaQuery.of(context).size.width,
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AppLargeText(
+                                text: detail.places.name,
+                                color: Colors.black.withOpacity(0.8),
+                              ),
+                              AppLargeText(
+                                text: "\$" + detail.places.price.toString(),
+                                color: AppColors.mainColor,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                color: AppColors.mainColor,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              AppText(
+                                text: detail.places.location,
+                                color: AppColors.textColor1,
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Wrap(
+                                children: List.generate(5, (index) {
+                                  return Icon(
+                                    Icons.star,
+                                    color: index < detail.places.stars
+                                        ? AppColors.starColor
+                                        : AppColors.textColor2,
+                                    size: 20,
+                                  );
+                                }),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              AppText(text: "(5.0)"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          AppLargeText(
+                            text: "People",
+                            color: Colors.black.withOpacity(0.8),
+                            size: 20,
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          AppText(
+                            text: "Number of people in your group",
+                            color: AppColors.mainTextColor,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Wrap(
+                            children: List.generate(5, (index) {
+                              return InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    selectedIndex = index;
+                                  });
+                                },
+                                child: Container(
+                                  margin: const EdgeInsets.only(right: 10),
+                                  child: AppButtons(
+                                    size: 50,
+                                    color: selectedIndex == index
+                                        ? Colors.white
+                                        : Colors.black,
+                                    backgroundColor: selectedIndex == index
+                                        ? Colors.black.withOpacity(0.7)
+                                        : Colors.white,
+                                    borderColor: selectedIndex == index
+                                        ? Colors.black.withOpacity(0.5)
+                                        : Colors.white,
+                                    text: (index + 1).toString(),
+                                    // icon: Icons.favorite,
+                                    // isIcon: true,
+                                  ),
+                                ),
+                              );
+                            }),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          AppLargeText(
+                            text: "Description",
+                            color: Colors.black.withOpacity(0.8),
+                            size: 20,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          AppText(
+                            text: detail.places.description,
+                            color: AppColors.mainTextColor,
+                            size: 14,
+                          ),
+                        ]),
+                  )),
+              Positioned(
+                bottom: 20,
                 left: 20,
-                top: 60,
+                right: 20,
                 child: Row(
                   children: [
-                    IconButton(
-                      onPressed: () {
-                        BlocProvider.of<AppCubit>(context).goHome();
-                      },
-                      icon: Icon(Icons.menu),
-                      color: Colors.white,
+                    AppButtons(
+                        size: 60,
+                        color: AppColors.textColor1,
+                        backgroundColor: Colors.white,
+                        borderColor: AppColors.textColor1,
+                        isIcon: true,
+                        icon: Icons.favorite),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    ResponsiveButton(
+                      isResponsive: true,
                     )
                   ],
-                )),
-            Positioned(
-                top: 260,
-                child: Container(
-                  padding: EdgeInsets.only(left: 20, right: 20, top: 20),
-                  width: MediaQuery.of(context).size.width,
-                  height: 500,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40),
-                    ),
-                  ),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            AppLargeText(
-                              text: "Himalaya ,",
-                              color: Colors.black.withOpacity(0.8),
-                            ),
-                            AppLargeText(
-                              text: "\$250",
-                              color: AppColors.mainColor,
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on,
-                              color: AppColors.mainColor,
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            AppText(
-                              text: "Jammu And Kashmir",
-                              color: AppColors.textColor1,
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Wrap(
-                              children: List.generate(5, (index) {
-                                return Icon(
-                                  Icons.star,
-                                  color: index < gottenStars
-                                      ? AppColors.starColor
-                                      : AppColors.textColor2,
-                                  size: 20,
-                                );
-                              }),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            AppText(text: "(4.0)"),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        AppLargeText(
-                          text: "People",
-                          color: Colors.black.withOpacity(0.8),
-                          size: 20,
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        AppText(
-                          text: "Number of people in your group",
-                          color: AppColors.mainTextColor,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Wrap(
-                          children: List.generate(5, (index) {
-                            return InkWell(
-                              onTap: () {
-                                setState(() {
-                                  selectedIndex = index;
-                                });
-                              },
-                              child: Container(
-                                margin: const EdgeInsets.only(right: 10),
-                                child: AppButtons(
-                                  size: 50,
-                                  color: selectedIndex == index
-                                      ? Colors.white
-                                      : Colors.black,
-                                  backgroundColor: selectedIndex == index
-                                      ? Colors.black.withOpacity(0.7)
-                                      : Colors.white,
-                                  borderColor: selectedIndex == index
-                                      ? Colors.black.withOpacity(0.5)
-                                      : Colors.white,
-                                  text: (index + 1).toString(),
-                                  // icon: Icons.favorite,
-                                  // isIcon: true,
-                                ),
-                              ),
-                            );
-                          }),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        AppLargeText(
-                          text: "Description",
-                          color: Colors.black.withOpacity(0.8),
-                          size: 20,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        AppText(
-                          text:
-                              "  Traveling is one of the best ways to learn more about yourself.,You'll come home knowing yourself better, and with a fresh perspective on what you want out of life.",
-                          color: AppColors.mainTextColor,
-                          size: 14,
-                        ),
-                      ]),
-                )),
-            Positioned(
-              bottom: 20,
-              left: 20,
-              right: 20,
-              child: Row(
-                children: [
-                  AppButtons(
-                      size: 60,
-                      color: AppColors.textColor1,
-                      backgroundColor: Colors.white,
-                      borderColor: AppColors.textColor1,
-                      isIcon: true,
-                      icon: Icons.favorite),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  ResponsiveButton(
-                    isResponsive: true,
-                  )
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
